01315a990c34d20932203027f735794994313449
Manoj Sethi <msethi@qubittech.co.uk>
2019-02-27 10:22:49 +0000

    working for simple ones

--- solution.js
+++ solution.js
@@ -4,54 +4,72 @@ function intToArr(num) {
 
 function findSmaller(num) {
   const numArr = intToArr(num);
-  let result = -1;
-  if (
-    numArr.length === 1 ||
-    numArr.join('') ===
-      numArr
-        .slice()
-        .sort()
-        .join('')
-  ) {
-    return result;
-  }
+
+  // if (
+  //   numArr.length === 1 ||
+  //   numArr.join('') ===
+  //     numArr
+  //       .slice()
+  //       .sort()
+  //       .join('')
+  // ) {
+  //   return result;
+  // }
 
   const lengthOfArr = numArr.length;
+  let biggerResult;
   const biggerDigits = [];
-  let x = numArr.slice();
 
   for (let i = lengthOfArr - 1; i >= 0; i--) {
     const numberBeingChecked = numArr[i];
     const filteredArray = numArr.filter((n, j) => j < i);
-    const biggerResult = anyBigger(filteredArray, numberBeingChecked);
+    biggerResult = anyBigger(filteredArray, numberBeingChecked, biggerDigits);
 
-    if (!biggerResult) {
-      biggerDigits.push(numberBeingChecked);
-    } else {
-      let arrResult = [];
-      for (let j = 0; j < filteredArray.length; j++) {
-        if (j === biggerResult.indexOfDigit) {
-          arrResult.push(numberBeingChecked);
-          arrResult = [...arrResult, ...biggerDigits];
-        } else {
-        }
-        arrResult.push(filteredArray[j]);
-      }
-
-      const joinedNumber = arrResult.join('');
-      result = joinedNumber.startsWith('0') ? -1 : joinedNumber;
+    if (biggerResult) {
+      biggerResult = { ...biggerResult, foundIndex: i };
       break;
-    }
+    } else biggerDigits.push(numberBeingChecked);
   }
+  console.log(biggerResult);
+  if (biggerResult) {
+    const { indexOfDigit, numberBeingChecked, foundIndex, smallerDigits } = biggerResult;
+    let originalNumber = numArr.slice();
+    let result = [];
+    // if (smallerDigits.length > 0) {
+    //   console.log(...originalNumber.slice(0, indexOfDigit + 1));
+    //   result = [...originalNumber.slice(0, indexOfDigit)].join('');
+    // } else {
+    result = numArr.slice();
+    const numberAtFoundDigit = result[indexOfDigit];
+    result[indexOfDigit] = numberBeingChecked;
+    result[foundIndex] = numberAtFoundDigit;
+    result = [
+      ...result.slice(0, indexOfDigit + 1),
+      ...result
+        .slice(indexOfDigit + 1, result.length)
+        .sort()
+        .reverse(),
+    ].join('');
+    // }
+    console.log(result);
 
-  return Number(result);
+    return result.startsWith('0') ? -1 : Number(result);
+  }
+  return -1;
 }
 
-function anyBigger(arr, number) {
+function anyBigger(arr, number, existingFailures) {
   const result = null;
-
+  const smallerDigits = [];
   for (i = arr.length - 1; i >= 0; i--) {
-    if (number < arr[i]) return { resultantDigit: arr[i], indexOfDigit: i };
+    // if (existingFailures.length === 0) {
+    if (number < arr[i]) return { resultantDigit: arr[i], indexOfDigit: i, numberBeingChecked: number, smallerDigits };
+    // } else {
+    //   // console.log('in else', existingFailures, number, arr[i]);
+    //   if (number <= arr[i])
+    //     return { resultantDigit: arr[i], indexOfDigit: i, numberBeingChecked: number, smallerDigits };
+    //   else smallerDigits.push(arr[i]);
+    // }
   }
 
   return result;
