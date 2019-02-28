60d442656a31ee38673d6ab5c8bce17383d871f1
Manoj Sethi <neoms21@gmail.com>
2019-02-28 14:10:07 +0000

    Working Sol. Kata Complete

--- parseInt/solution.js
+++ parseInt/solution.js
@@ -1,4 +1,5 @@
 const singles = {
+  zero: 0,
   one: 1,
   two: 2,
   three: 3,
@@ -9,9 +10,8 @@ const singles = {
   eight: 8,
   nine: 9,
   ten: 10,
-
   eleven: 11,
-  tweleve: 12,
+  twelve: 12,
   thirteen: 13,
   fourteen: 14,
   fifteen: 15,
@@ -19,12 +19,12 @@ const singles = {
   seventeen: 17,
   eighteen: 18,
   nineteen: 19,
-  twenty: 20,
 };
 
 const tens = {
+  twenty: 20,
   thirty: 30,
-  fourty: 40,
+  forty: 40,
   fifty: 50,
   sixty: 60,
   seventy: 70,
@@ -33,51 +33,72 @@ const tens = {
 };
 
 const others = {
-  hundred: 100,
-  thousand: 1000,
-  million: 1000000,
+  hundred: {value :100, order:1},
+  thousand: {value:1000, order:2},
+  million: {value:1000000, order:3},
 };
-
-// const operations = {
-//   M,
-//   A,
-// };
 
 function parseInt(words) {
   const strippedWords = words
-    .replace(/ and /, ' ')
-    .toLowerCase()
-    .split(' ');
+    .split(' ').filter(w=> w!=='and').map(x=>x.toLowerCase());
+    // console.log(strippedWords);
   let result = 0;
+  let currentWordAlreadyAdded = false;
+  let higherOrderCompleted = undefined;
+  const collections = groupInCollections(strippedWords);
+  // console.log(collections);
 
-  for (let i = 0; i < strippedWords.length; i++) {
-    const currentWord = findWhichCollection(strippedWords[i]);
-    const nextWord = strippedWords[i + 1] ? findWhichCollection(strippedWords[i + 1]) : undefined;
-
-    console.log(`Stripped: ${strippedWords[i]} - ${JSON.stringify(currentWord)}`);
-    console.log(`Next: ${strippedWords[i + 1]} - ${JSON.stringify(nextWord)}`);
-
-    if (nextWord && nextWord.higher) {
-      const cw = currentWord.single ? currentWord.single : currentWord.ten;
-      result += cw * nextWord.higher;
-    } else if (nextWord && nextWord.single) {
-      const cw = currentWord.ten ? currentWord.ten : currentWord.single;
-      result += cw + nextWord.single;
+  collections.forEach( (c, i) => {
+    // console.log(c)
+    if (c.isHigher) {
+        if(higherOrderCompleted > c.order)
+          {
+            result = result - collections[i-1].value;
+            result += collections[i-1].value * c.value;
+          }
+       else{
+           result = result * c.value;
+           higherOrderCompleted = c.order;}
+    } else {
+       result += c.value;
     }
+  });
 
-    if (nextWord && nextWord.higher) ++i;
-  }
-
-  console.log(result);
   return result;
 }
 
-function findWhichCollection(word) {
-  return {
-    single: singles[word],
-    ten: tens[word],
-    higher: others[word],
-  };
+function groupInCollections(words) {
+  const result = [];
+  const collection = [];
+  words.forEach(w => {
+    const val = getValue(w);
+    const obj = { key: w, value: val.value, isHigher: val.isHigher, order: val.order };
+
+    collection.push(obj);
+  });
+  //   let innerCollections = [];
+  //   collection.forEach(c => {
+  //     if (!c.isHigher) {
+  //       innerCollections = [];
+  //       result.push(innerCollections);
+  //     }
+  //     innerCollections.push(c);
+  //   });
+
+  return collection;
+}
+
+function getValue(word) {
+  if (word.indexOf('-') !== -1) {
+    const combinedWords = word.split('-');
+    return {value:tens[combinedWords[0]] + singles[combinedWords[1]]};
+  }
+
+  if (singles[word] !== undefined) return {value: singles[word]};
+
+  if (tens[word]) return {value: tens[word]};
+
+  if (others[word]) return {value:others[word].value, isHigher: true, order: others[word].order};
 }
 
 module.exports = parseInt;
